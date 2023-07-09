87bc2649534216eca403da69928fdf4b2010ab2b
patrickRDuck <133723328+patrickRDuck@users.noreply.github.com>
2023-07-08 22:47:22 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        
+
         //Ajuste para telas de smartphone
         if(window.innerWidth < 450) {
             window.scrollTo({top: 0, behavior: 'smooth'});
@@ -16,7 +16,7 @@ personagens.forEach((personagem) => {
         //Trocando a imagem maior do personagem de acordo com o selecionado
         const imagemPersonagemGrande = document.querySelector('.personagem-grande')
         const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `../src/imagens/card-${idPersonagem}.png`;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
 
         //Trocando a descrição do personagem que está selecionado
         const nomePersonagem = document.getElementById('nome-personagem');
